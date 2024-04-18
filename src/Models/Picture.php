643351b70b65fe51ec72995827ac88579381177e
PS196391 <ps196391@edu.summacollege.nl>
2024-04-18 10:32:31 +0200

    Bugs fixed

--- src/Models/Picture.php
+++ src/Models/Picture.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Danir\MediaLib\Models\Picture;
+namespace Danir\MediaLib\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
--- src/Models/Tag.php
+++ src/Models/Tag.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Danir\MediaLib\Models\Tag;
+namespace Danir\MediaLib\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
